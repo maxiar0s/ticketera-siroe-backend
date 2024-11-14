fd9d0342f836d7d9e64b72bf348122ad7d129161
JMunozSiroe <jmunoz@siroe.cl>
2024-11-14 15:52:30 -0300

    prueba

--- netlify/functions/levantamientos.js
+++ netlify/functions/levantamientos.js
@@ -1,12 +1,11 @@
 // netlify/functions/levantamientos.js
+import { levantamiento } from '../../models/index.js'; // Usa `require` en lugar de `import`
 
-export async function handler(event, context) {
-    const { levantamiento } = require('../../models/index.js');
+export async function handler(event, context) { // Usa `module.exports` para exportar la función
     const levantamientos = await levantamiento.findAll();
   
     return {
-      statusCode: 200,
-      body: JSON.stringify(levantamientos)
+        statusCode: 200,
+        body: JSON.stringify(levantamientos),
     };
 }
-  
