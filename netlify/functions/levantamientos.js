65ebf89525a137355153a1b26b768ebda282e987
JMunozSiroe <jmunoz@siroe.cl>
2024-11-14 15:30:22 -0300

    new toml

--- netlify/functions/levantamientos.js
+++ netlify/functions/levantamientos.js
@@ -1,13 +1,12 @@
 // netlify/functions/levantamientos.js
-import { levantamiento } from '../../models/index.js';
+const { levantamiento } = require('../../models/index.js');
 
 exports.handler = async function(event, context) {
-
     const levantamients = await levantamiento.findAll();
   
     return {
       statusCode: 200,
       body: JSON.stringify(levantamients)
     };
-  };
+};
   
