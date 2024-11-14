88ccd2ce4417869ba85f610761943b83ad6e6f11
JMunozSiroe <jmunoz@siroe.cl>
2024-11-14 16:34:07 -0300

    prueba

--- netlify/functions/levantamientos.mjs
+++ netlify/functions/levantamientos.mjs
@@ -1,11 +1,11 @@
-// netlify/functions/levantamientos.js
+// netlify/functions/levantamientos.mjs
 import { levantamiento } from '../../models/index.js';
 
 export async function handler(event, context) {
     const levantamientos = await levantamiento.findAll();
-  
+
     return {
         statusCode: 200,
         body: JSON.stringify(levantamientos),
     };
-};
+}
