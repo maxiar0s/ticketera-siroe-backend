9ba735c54bc9bb2f677bfb1f0c126728fa279bac
JMunozSiroe <jmunoz@siroe.cl>
2024-11-14 16:38:01 -0300

    prueba

--- netlify/functions/levantamientos.js
+++ netlify/functions/levantamientos.js
@@ -1,5 +1,6 @@
 // netlify/functions/levantamientos.mjs
-import { levantamiento } from '../../models/index.js';
+// import { levantamiento } from '../../models/index.js';
+import fetch from 'node-fetch';
 
 export async function handler(event, context) {
     const levantamientos = await levantamiento.findAll();
