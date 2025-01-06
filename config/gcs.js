9f58185405fd7313cf4cc10b092fe1b7f34bfa41
JMunozSiroe <jmunoz@siroe.cl>
2025-01-06 19:10:56 -0300

    changes

--- config/gcs.js
+++ config/gcs.js
@@ -1,12 +1,8 @@
 import { Storage } from '@google-cloud/storage';
 
-const credentials = JSON.parse(
-  Buffer.from(process.env.GCS_CREDENTIALS_BASE64, 'base64').toString('utf-8')
-);
-
 const storage = new Storage({
   projectId: process.env.GCLOUD_PROJECT_ID,
-  credentials: credentials
+  keyFilename: process.env.GCLOUD_KEY_FILE
 });
 
 const bucket = storage.bucket(process.env.GCLOUD_BUCKET_NAME);
--- controller/apiController.js
+++ controller/apiController.js
@@ -446,7 +446,6 @@ const getResults = async (req, res) => {
         CasaMatrizModel.count()
     ])
 
-    
     const paginas = Math.ceil(total / limit);
     paginaActual = Number(paginaActual);
     res.json({clientes, paginas, paginaActual});
