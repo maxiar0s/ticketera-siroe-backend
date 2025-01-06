355cead2e81505ea233110074654ce543696d5b4
JMunozSiroe <jmunoz@siroe.cl>
2025-01-06 16:31:38 -0300

    changes

--- config/gcs.js
+++ config/gcs.js
@@ -1,8 +1,12 @@
 import { Storage } from '@google-cloud/storage';
 
+const credentials = JSON.parse(
+  Buffer.from(process.env.GCS_CREDENTIALS_BASE64, 'base64').toString('utf-8')
+);
+
 const storage = new Storage({
   projectId: process.env.GCLOUD_PROJECT_ID,
-  keyFilename: process.env.GCLOUD_KEY_FILE
+  credentials: credentials
 });
 
 const bucket = storage.bucket(process.env.GCLOUD_BUCKET_NAME);
