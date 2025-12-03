9c800c29f60bcc2a7f017668f07fbb99e19e79b3
maxiar0s <maxi.aros.osorio@gmail.com>
2025-12-03 10:53:41 -0300

    changes seeder

--- seed/Cuenta.js
+++ seed/Cuenta.js
@@ -1,14 +1,14 @@
-import bcrypt from 'bcrypt';
+import bcrypt from "bcrypt";
 
 const Cuentas = [
-    {
-        name: 'Jonathan',
-        email: 'jmunoz@siroe.cl',
-        telefono: 968708503,
-        password: bcrypt.hashSync('password', 10),
-        tipoCuentaId: 1,
-        estadoCuentaId: 1
-    },
-]
+  {
+    name: "Maximiliano",
+    email: "maros@soportesiroe.cl",
+    telefono: 968708503,
+    password: bcrypt.hashSync("password", 10),
+    tipoCuentaId: 1,
+    estadoCuentaId: 1,
+  },
+];
 
 export default Cuentas;
